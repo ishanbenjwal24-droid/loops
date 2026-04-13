4506cc0519717adace97d6eda99a6ce7744af96b
Ishan <ishanbenjwal24@gmail.com>
2026-04-13 19:06:21 +0000

    mario

--- cat.c
+++ cat.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
-#include <cs50.h>
+#include<stdio.h>
+
 int main(void)
 {
-    int n = get_int("what's n?");
-    for (int i = 0; i < n;i++)
-    printf("pikachu\n");
+    for ( int row = 0;  row < 3; row++)
+    {
+        for (int column = 0; column < 3; column++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
